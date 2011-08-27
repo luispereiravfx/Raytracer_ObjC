6f27a42ea07c71e2409c79a8fceb5f417e034821
Luis Pereira <luisparadapereira@Luis-Pereiras-MacBook-Pro.local>
2011-08-27 20:05:32 +0100

    Initial Commit

---
+++ Raytracer_v01/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Raytracer_v01
+//
+//  Created by Luis Pereira on 27/08/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
